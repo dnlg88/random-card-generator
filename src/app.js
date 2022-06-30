5362dc091ec1a09412144f2c9bbe33df8dab649b
Daniel Guzmán <dnlguzman@gmail.com>
2022-06-30 17:19:20 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let random1 = Math.floor(Math.random() * 12);
+  let random2 = Math.floor(Math.random() * 3);
+  let simbolo = ["♦", "♥", "♠", "♣"];
+
+  let pinta = document.querySelectorAll(".pinta");
+  for (let i = 0; i < pinta.length; i++) {
+    pinta[i].innerText = simbolo[random2];
+  }
+  if (pinta[0].innerText == "♦" || pinta[0].innerText == "♥") {
+    for (let i = 0; i < pinta.length; i++) {
+      pinta[i].style.color = "red";
+    }
+  }
+  let numero = document.querySelectorAll(".numero");
+  for (let i = 0; i < numero.length; i++) {
+    numero[i].innerText = numeros[random1];
+  }
 };
